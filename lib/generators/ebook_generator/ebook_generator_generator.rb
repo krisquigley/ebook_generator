708046c40d4fec3002268b31d95f71a5e760a7c0
Kris Quigley <kris@krisquigley.co.uk>
2014-08-30 22:07:02 +0100

    updated generator to extend from activerecord to support rails 4 migration method

--- lib/generators/ebook_generator/ebook_generator_generator.rb
+++ lib/generators/ebook_generator/ebook_generator_generator.rb
@@ -2,13 +2,12 @@ require 'rails/generators'
 require 'rails/generators/migration'
 require 'rails/generators/active_record'
 
-class EbookGeneratorGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-  
-  include Rails::Generators::Migration
-  extend ActiveRecord::Generators::Migration
+class EbookGeneratorGenerator < ActiveRecord::Generators::Base
+  argument :name, type: :string, default: 'random_name'
 
-  def create_model_file
+  source_root File.expand_path('../templates', __FILE__)
+
+  def create_model_files
     template "ebook.rb", "app/models/ebook.rb"
     template "section.rb", "app/models/section.rb"
     migration_template "create_ebooks.rb", "db/migrate/create_ebooks.rb"
