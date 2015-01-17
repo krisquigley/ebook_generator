068910debe2dcd48894a65f223f1d64d256c622c
Kris Quigley <kris@krisquigley.co.uk>
2015-01-17 17:58:01 +0000

    Beginning foundations for html_to_ebook class

---
+++ lib/ebook_generator/html_to_ebook.rb
@@ -0,0 +1,19 @@
+require 'nokogiri'
+
+class HtmlToEbook
+  def initialize(file)
+    @ebook = Ebook.new
+    f = File.open(file)
+    @html = Nokogiri::HTML(f)
+    f.close
+  end
+
+  def set_title(title_tag = "title")
+    title = @html.css_at(title_tag).content
+    @ebook.title = title
+  def 
+
+  def save_ebook
+    @ebook.save
+  end
+end
--- lib/tasks/ebook_generator_tasks.rake
+++ lib/tasks/ebook_generator_tasks.rake
@@ -1,4 +1,8 @@
-# desc "Explaining what the task does"
-# task :ebook_generator do
-#   # Task goes here
-# end
+namespace :ebook_generator do
+  desc "Generate ebook from html"
+  task :to_html, :file do |t, args|
+    # Can be passed a single file or comma separated list
+    files = args[:file].split(',')
+    
+  end
+end
